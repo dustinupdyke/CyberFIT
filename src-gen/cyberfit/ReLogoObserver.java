61c8c7184c76dbf9e791ed3534ffc00e2d255eca
dustinupdyke <dustin@updyke.com>
2018-06-21 14:15:16 -0400

    Init

---
+++ src-gen/cyberfit/ReLogoObserver.java
@@ -0,0 +1,261 @@
+package cyberfit;
+
+import static repast.simphony.relogo.Utility.*;
+import static repast.simphony.relogo.UtilityG.*;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+import groovy.lang.Closure;
+import repast.simphony.relogo.*;
+import repast.simphony.relogo.builder.GeneratedByReLogoBuilder;
+import repast.simphony.relogo.builder.ReLogoBuilderGeneratedFor;
+
+@GeneratedByReLogoBuilder
+@SuppressWarnings({"unused","rawtypes","unchecked"})
+public class ReLogoObserver extends BaseObserver{
+
+	/**
+	 * Makes a number of randomly oriented userTurtles and then executes a set of commands on the
+	 * created userTurtles.
+	 * 
+	 * @param number
+	 *            a number
+	 * @param closure
+	 *            a set of commands
+	 * @return created userTurtles
+	 */
+	@ReLogoBuilderGeneratedFor("cyberfit.relogo.UserTurtle")
+	public AgentSet<cyberfit.relogo.UserTurtle> createUserTurtles(int number, Closure closure) {
+		AgentSet<cyberfit.relogo.UserTurtle> result = new AgentSet<>();
+		AgentSet<Turtle> createResult = this.crt(number,closure,"UserTurtle");
+		for (Turtle t : createResult){
+			if (t instanceof cyberfit.relogo.UserTurtle){
+				result.add((cyberfit.relogo.UserTurtle)t);
+			}
+		} 
+		return result; 
+	}
+
+	/**
+	 * Makes a number of randomly oriented userTurtles and then executes a set of commands on the
+	 * created userTurtles.
+	 * 
+	 * @param number
+	 *            a number
+	 * @param closure
+	 *            a set of commands
+	 * @return created userTurtles
+	 */
+	@ReLogoBuilderGeneratedFor("cyberfit.relogo.UserTurtle")
+	public AgentSet<cyberfit.relogo.UserTurtle> createUserTurtles(int number) {
+		return createUserTurtles(number,null);
+	}
+
+	/**
+	 * Makes a number of uniformly fanned userTurtles and then executes a set of commands on the
+	 * created userTurtles.
+	 * 
+	 * @param number
+	 *            a number
+	 * @param closure
+	 *            a set of commands
+	 * @return created userTurtles
+	 */
+	@ReLogoBuilderGeneratedFor("cyberfit.relogo.UserTurtle")
+	public AgentSet<cyberfit.relogo.UserTurtle> createOrderedUserTurtles(int number, Closure closure) {
+		AgentSet<cyberfit.relogo.UserTurtle> result = new AgentSet<>();
+		AgentSet<Turtle> createResult = this.cro(number,closure,"UserTurtle");
+		for (Turtle t : createResult){
+			if (t instanceof cyberfit.relogo.UserTurtle){
+				result.add((cyberfit.relogo.UserTurtle)t);
+			}
+		} 
+		return result; 
+	}
+
+	/**
+	 * Makes a number of uniformly fanned userTurtles and then executes a set of commands on the
+	 * created userTurtles.
+	 * 
+	 * @param number
+	 *            a number
+	 * @param closure
+	 *            a set of commands
+	 * @return created userTurtles
+	 */
+	@ReLogoBuilderGeneratedFor("cyberfit.relogo.UserTurtle")
+	public AgentSet<cyberfit.relogo.UserTurtle> createOrderedUserTurtles(int number) {
+		return createOrderedUserTurtles(number,null);
+	}
+
+	/**
+	 * Queries if object is a userTurtle.
+	 * 
+	 * @param o
+	 *            an object
+	 * @return true or false based on whether the object is a userTurtle
+	 */
+	@ReLogoBuilderGeneratedFor("cyberfit.relogo.UserTurtle")
+	public boolean isUserTurtleQ(Object o){
+		return (o instanceof cyberfit.relogo.UserTurtle);
+	}
+
+	/**
+	 * Returns an agentset containing all userTurtles.
+	 * 
+	 * @return agentset of all userTurtles
+	 */
+	@ReLogoBuilderGeneratedFor("cyberfit.relogo.UserTurtle")
+	public AgentSet<cyberfit.relogo.UserTurtle> userTurtles(){
+		AgentSet<cyberfit.relogo.UserTurtle> a = new AgentSet<cyberfit.relogo.UserTurtle>();
+		for (Object e : this.getContext().getObjects(cyberfit.relogo.UserTurtle.class)) {
+			if (e instanceof cyberfit.relogo.UserTurtle){
+				a.add((cyberfit.relogo.UserTurtle)e);
+			}
+		}
+		return a;
+	}
+
+	/**
+	 * Returns the userTurtle with the given who number.
+	 * 
+	 * @param number
+	 *            a number
+	 * @return turtle number
+	 */
+	@ReLogoBuilderGeneratedFor("cyberfit.relogo.UserTurtle")
+	public cyberfit.relogo.UserTurtle userTurtle(Number number){
+		Turtle turtle = Utility.turtleU(number.intValue(), this);
+		if (turtle instanceof cyberfit.relogo.UserTurtle)
+			return (cyberfit.relogo.UserTurtle) turtle;
+		return null;
+	}
+
+	/**
+	 * Returns an agentset of userTurtles on a given patch.
+	 * 
+	 * @param p
+	 *            a patch
+	 * @return agentset of userTurtles on patch p
+	 */
+	@ReLogoBuilderGeneratedFor("cyberfit.relogo.UserTurtle")
+	public AgentSet<cyberfit.relogo.UserTurtle> userTurtlesOn(Patch p){
+		AgentSet<cyberfit.relogo.UserTurtle> result = new AgentSet<cyberfit.relogo.UserTurtle>();						
+		for (Turtle t : Utility.getTurtlesOnGridPoint(p.getGridLocation(),this,"userTurtle")){
+			if (t instanceof cyberfit.relogo.UserTurtle)
+			result.add((cyberfit.relogo.UserTurtle)t);
+		}
+		return result;
+	}
+
+	/**
+	 * Returns an agentset of userTurtles on the same patch as a turtle.
+	 * 
+	 * @param t
+	 *            a turtle
+	 * @return agentset of userTurtles on the same patch as turtle t
+	 */
+	@ReLogoBuilderGeneratedFor("cyberfit.relogo.UserTurtle")
+	public AgentSet<cyberfit.relogo.UserTurtle> userTurtlesOn(Turtle t){
+		AgentSet<cyberfit.relogo.UserTurtle> result = new AgentSet<cyberfit.relogo.UserTurtle>();						
+		for (Turtle tt : Utility.getTurtlesOnGridPoint(Utility.ndPointToGridPoint(t.getTurtleLocation()),this,"userTurtle")){
+			if (tt instanceof cyberfit.relogo.UserTurtle)
+			result.add((cyberfit.relogo.UserTurtle)tt);
+		}
+		return result;
+	}
+
+	/**
+	 * Returns an agentset of userTurtles on the patches in a collection or on the patches
+	 * that a collection of turtles are.
+	 * 
+	 * @param a
+	 *            a collection
+	 * @return agentset of userTurtles on the patches in collection a or on the patches
+	 *         that collection a turtles are
+	 */
+	@ReLogoBuilderGeneratedFor("cyberfit.relogo.UserTurtle")
+	public AgentSet<cyberfit.relogo.UserTurtle> userTurtlesOn(Collection c){
+
+		if (c == null || c.isEmpty()){
+			return new AgentSet<cyberfit.relogo.UserTurtle>();
+		}
+
+		Set<cyberfit.relogo.UserTurtle> total = new HashSet<cyberfit.relogo.UserTurtle>();
+		if (c.iterator().next() instanceof Turtle){
+			for (Object o : c){
+				if (o instanceof Turtle){
+					Turtle t = (Turtle) o;
+					total.addAll(userTurtlesOn(t));
+				}
+			}
+		}
+		else {
+			for (Object o : c){
+				if (o instanceof Patch){
+					Patch p = (Patch) o;
+					total.addAll(userTurtlesOn(p));
+				}
+			}
+		}
+		return new AgentSet<cyberfit.relogo.UserTurtle>(total);
+	}
+
+	/**
+	 * Queries if object is a userLink.
+	 * 
+	 * @param o
+	 *            an object
+	 * @return true or false based on whether the object is a userLink
+	 */
+	@ReLogoBuilderGeneratedFor("cyberfit.relogo.UserLink")
+	public boolean isUserLinkQ(Object o){
+		return (o instanceof cyberfit.relogo.UserLink);
+	}
+
+	/**
+	 * Returns an agentset containing all userLinks.
+	 * 
+	 * @return agentset of all userLinks
+	 */
+	@ReLogoBuilderGeneratedFor("cyberfit.relogo.UserLink")
+	public AgentSet<cyberfit.relogo.UserLink> userLinks(){
+		AgentSet<cyberfit.relogo.UserLink> a = new AgentSet<cyberfit.relogo.UserLink>();
+		for (Object e : this.getContext().getObjects(cyberfit.relogo.UserLink.class)) {
+			if (e instanceof cyberfit.relogo.UserLink){
+				a.add((cyberfit.relogo.UserLink)e);
+			}
+		}
+		return a;
+	}
+
+	/**
+	 * Returns the userLink between two turtles.
+	 * 
+	 * @param oneEnd
+	 *            an integer
+	 * @param otherEnd
+	 *            an integer
+	 * @return userLink between two turtles
+	 */
+	@ReLogoBuilderGeneratedFor("cyberfit.relogo.UserLink")
+	public cyberfit.relogo.UserLink userLink(Number oneEnd, Number otherEnd) {
+		return (cyberfit.relogo.UserLink)(this.getNetwork("UserLink").getEdge(turtle(oneEnd),turtle(otherEnd)));
+	}
+
+	/**
+	 * Returns the userLink between two turtles.
+	 * 
+	 * @param oneEnd
+	 *            a turtle
+	 * @param otherEnd
+	 *            a turtle
+	 * @return userLink between two turtles
+	 */
+	@ReLogoBuilderGeneratedFor("cyberfit.relogo.UserLink")
+	public cyberfit.relogo.UserLink userLink(Turtle oneEnd, Turtle otherEnd) {
+		return userLink(oneEnd.getWho(), otherEnd.getWho());
+	}
+
+
+}
---
+++ src-gen/cyberfit/ReLogoPatch.java
@@ -0,0 +1,275 @@
+package cyberfit;
+
+import static repast.simphony.relogo.Utility.*;
+import static repast.simphony.relogo.UtilityG.*;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+import groovy.lang.Closure;
+import repast.simphony.relogo.*;
+import repast.simphony.relogo.ast.Diffusible;
+import repast.simphony.relogo.builder.GeneratedByReLogoBuilder;
+import repast.simphony.relogo.builder.ReLogoBuilderGeneratedFor;
+import repast.simphony.space.SpatialException;
+import repast.simphony.space.grid.Grid;
+import repast.simphony.space.grid.GridPoint;
+
+@GeneratedByReLogoBuilder
+@SuppressWarnings({"unused","rawtypes","unchecked"})
+public class ReLogoPatch extends BasePatch{
+
+	/**
+	 * Sprouts (makes) a number of new userTurtles and then executes a set of commands on the
+	 * created userTurtles.
+	 * 
+	 * @param number
+	 *            a number
+	 * @param closure
+	 *            a set of commands
+	 * @return created userTurtles
+	 */
+	@ReLogoBuilderGeneratedFor("cyberfit.relogo.UserTurtle")
+	public AgentSet<cyberfit.relogo.UserTurtle> sproutUserTurtles(int number, Closure closure) {
+		AgentSet<cyberfit.relogo.UserTurtle> result = new AgentSet<>();
+		AgentSet<Turtle> createResult = this.sprout(number,closure,"UserTurtle");
+		for (Turtle t : createResult){
+			if (t instanceof cyberfit.relogo.UserTurtle){
+				result.add((cyberfit.relogo.UserTurtle)t);
+			}
+		} 
+		return result;
+	}
+
+	/**
+	 * Sprouts (makes) a number of new userTurtles and then executes a set of commands on the
+	 * created userTurtles.
+	 * 
+	 * @param number
+	 *            a number
+	 * @param closure
+	 *            a set of commands
+	 * @return created userTurtles
+	 */
+	@ReLogoBuilderGeneratedFor("cyberfit.relogo.UserTurtle")
+	public AgentSet<cyberfit.relogo.UserTurtle> sproutUserTurtles(int number) {
+		return sproutUserTurtles(number,null);
+	}
+
+	/**
+	 * Returns an agentset of userTurtles from the patch of the caller.
+	 * 
+	 * @return agentset of userTurtles from the caller's patch
+	 */
+	@ReLogoBuilderGeneratedFor("cyberfit.relogo.UserTurtle")
+	public AgentSet<cyberfit.relogo.UserTurtle> userTurtlesHere(){
+	  Grid grid = getMyObserver().getGrid();
+	  GridPoint gridPoint = grid.getLocation(this);
+	  AgentSet<cyberfit.relogo.UserTurtle> result = new AgentSet<cyberfit.relogo.UserTurtle>();
+	  for (Turtle t : Utility.getTurtlesOnGridPoint(gridPoint,getMyObserver(),"userTurtle")){
+			if (t instanceof cyberfit.relogo.UserTurtle)
+			result.add((cyberfit.relogo.UserTurtle)t);
+		}
+		return result;
+	}
+
+	/**
+	 * Returns the agentset of userTurtles on the patch at the direction (ndx, ndy) from the
+	 * caller.
+	 * 
+	 * @param nX
+	 *            a number
+	 * @param nY
+	 *            a number
+	 * @returns agentset of userTurtles at the direction (nX, nY) from the caller
+	 */
+	@ReLogoBuilderGeneratedFor("cyberfit.relogo.UserTurtle")
+	public AgentSet<cyberfit.relogo.UserTurtle> userTurtlesAt(Number nX, Number nY){
+		double dx = nX.doubleValue();
+		double dy = nY.doubleValue();
+		double[] displacement = {dx,dy};
+
+		try{
+		GridPoint gridPoint = Utility.getGridPointAtDisplacement(getGridLocationAsNdPoint(),displacement,getMyObserver());
+		AgentSet<cyberfit.relogo.UserTurtle> result = new AgentSet<cyberfit.relogo.UserTurtle>();						
+		for (Turtle t : Utility.getTurtlesOnGridPoint(gridPoint,getMyObserver(),"userTurtle")){
+			if (t instanceof cyberfit.relogo.UserTurtle)
+			result.add((cyberfit.relogo.UserTurtle)t);
+		}
+		return result;
+		}
+		catch(SpatialException e){
+			return new AgentSet<cyberfit.relogo.UserTurtle>();
+		}
+	}
+
+	/**
+	 * Returns an agentset of userTurtles on a given patch.
+	 * 
+	 * @param p
+	 *            a patch
+	 * @return agentset of userTurtles on patch p
+	 */
+	@ReLogoBuilderGeneratedFor("cyberfit.relogo.UserTurtle")
+	public AgentSet<cyberfit.relogo.UserTurtle> userTurtlesOn(Patch p){
+		AgentSet<cyberfit.relogo.UserTurtle> result = new AgentSet<cyberfit.relogo.UserTurtle>();						
+		for (Turtle t : Utility.getTurtlesOnGridPoint(p.getGridLocation(),getMyObserver(),"userTurtle")){
+			if (t instanceof cyberfit.relogo.UserTurtle)
+			result.add((cyberfit.relogo.UserTurtle)t);
+		}
+		return result;
+	}
+
+	/**
+	 * Returns an agentset of userTurtles on the same patch as a turtle.
+	 * 
+	 * @param t
+	 *            a turtle
+	 * @return agentset of userTurtles on the same patch as turtle t
+	 */
+	@ReLogoBuilderGeneratedFor("cyberfit.relogo.UserTurtle")
+	public AgentSet<cyberfit.relogo.UserTurtle> userTurtlesOn(Turtle t){
+		AgentSet<cyberfit.relogo.UserTurtle> result = new AgentSet<cyberfit.relogo.UserTurtle>();						
+		for (Turtle tt : Utility.getTurtlesOnGridPoint(Utility.ndPointToGridPoint(t.getTurtleLocation()),getMyObserver(),"userTurtle")){
+			if (tt instanceof cyberfit.relogo.UserTurtle)
+			result.add((cyberfit.relogo.UserTurtle)tt);
+		}
+		return result;
+	}
+
+	/**
+	 * Returns an agentset of userTurtles on the patches in a collection or on the patches
+	 * that a collection of turtles are.
+	 * 
+	 * @param a
+	 *            a collection
+	 * @return agentset of userTurtles on the patches in collection a or on the patches
+	 *         that collection a turtles are
+	 */
+	@ReLogoBuilderGeneratedFor("cyberfit.relogo.UserTurtle")
+	public AgentSet<cyberfit.relogo.UserTurtle> userTurtlesOn(Collection c){
+
+		if (c == null || c.isEmpty()){
+			return new AgentSet<cyberfit.relogo.UserTurtle>();
+		}
+
+		Set<cyberfit.relogo.UserTurtle> total = new HashSet<cyberfit.relogo.UserTurtle>();
+		if (c.iterator().next() instanceof Turtle){
+			for (Object o : c){
+				if (o instanceof Turtle){
+					Turtle t = (Turtle) o;
+					total.addAll(userTurtlesOn(t));
+				}
+			}
+		}
+		else {
+			for (Object o : c){
+				if (o instanceof Patch){
+					Patch p = (Patch) o;
+					total.addAll(userTurtlesOn(p));
+				}
+			}
+		}
+		return new AgentSet<cyberfit.relogo.UserTurtle>(total);
+	}
+
+	/**
+	 * Queries if object is a userTurtle.
+	 * 
+	 * @param o
+	 *            an object
+	 * @return true or false based on whether the object is a userTurtle
+	 */
+	@ReLogoBuilderGeneratedFor("cyberfit.relogo.UserTurtle")
+	public boolean isUserTurtleQ(Object o){
+		return (o instanceof cyberfit.relogo.UserTurtle);
+	}
+
+	/**
+	 * Returns an agentset containing all userTurtles.
+	 * 
+	 * @return agentset of all userTurtles
+	 */
+	@ReLogoBuilderGeneratedFor("cyberfit.relogo.UserTurtle")
+	public AgentSet<cyberfit.relogo.UserTurtle> userTurtles(){
+		AgentSet<cyberfit.relogo.UserTurtle> a = new AgentSet<cyberfit.relogo.UserTurtle>();
+		for (Object e : this.getMyObserver().getContext().getObjects(cyberfit.relogo.UserTurtle.class)) {
+			if (e instanceof cyberfit.relogo.UserTurtle){
+				a.add((cyberfit.relogo.UserTurtle)e);
+			}
+		}
+		return a;
+	}
+
+	/**
+	 * Returns the userTurtle with the given who number.
+	 * 
+	 * @param number
+	 *            a number
+	 * @return turtle number
+	 */
+	@ReLogoBuilderGeneratedFor("cyberfit.relogo.UserTurtle")
+	public cyberfit.relogo.UserTurtle userTurtle(Number number){
+		Turtle turtle = Utility.turtleU(number.intValue(), getMyObserver());
+		if (turtle instanceof cyberfit.relogo.UserTurtle)
+			return (cyberfit.relogo.UserTurtle) turtle;
+		return null;
+	}
+
+	/**
+	 * Queries if object is a userLink.
+	 * 
+	 * @param o
+	 *            an object
+	 * @return true or false based on whether the object is a userLink
+	 */
+	@ReLogoBuilderGeneratedFor("cyberfit.relogo.UserLink")
+	public boolean isUserLinkQ(Object o){
+		return (o instanceof cyberfit.relogo.UserLink);
+	}
+
+	/**
+	 * Returns an agentset containing all userLinks.
+	 * 
+	 * @return agentset of all userLinks
+	 */
+	@ReLogoBuilderGeneratedFor("cyberfit.relogo.UserLink")
+	public AgentSet<cyberfit.relogo.UserLink> userLinks(){
+		AgentSet<cyberfit.relogo.UserLink> a = new AgentSet<cyberfit.relogo.UserLink>();
+		for (Object e : this.getMyObserver().getContext().getObjects(cyberfit.relogo.UserLink.class)) {
+			if (e instanceof cyberfit.relogo.UserLink){
+				a.add((cyberfit.relogo.UserLink)e);
+			}
+		}
+		return a;
+	}
+
+	/**
+	 * Returns the userLink between two turtles.
+	 * 
+	 * @param oneEnd
+	 *            an integer
+	 * @param otherEnd
+	 *            an integer
+	 * @return userLink between two turtles
+	 */
+	@ReLogoBuilderGeneratedFor("cyberfit.relogo.UserLink")
+	public cyberfit.relogo.UserLink userLink(Number oneEnd, Number otherEnd) {
+		return (cyberfit.relogo.UserLink)(this.getMyObserver().getNetwork("UserLink").getEdge(turtle(oneEnd),turtle(otherEnd)));
+	}
+
+	/**
+	 * Returns the userLink between two turtles.
+	 * 
+	 * @param oneEnd
+	 *            a turtle
+	 * @param otherEnd
+	 *            a turtle
+	 * @return userLink between two turtles
+	 */
+	@ReLogoBuilderGeneratedFor("cyberfit.relogo.UserLink")
+	public cyberfit.relogo.UserLink userLink(Turtle oneEnd, Turtle otherEnd) {
+		return userLink(oneEnd.getWho(), otherEnd.getWho());
+	}
+
+
+}

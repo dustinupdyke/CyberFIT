d2f6c6d477f9271863f915c89a23b4c125cb37cb
Geoffrey Dobson <30755162+gbdobson@users.noreply.github.com>
2018-07-03 13:51:06 -0400

    comments

--- src/cyberfit/relogo/Attacker.groovy
+++ src/cyberfit/relogo/Attacker.groovy
@@ -13,9 +13,9 @@ import repast.simphony.relogo.schedule.Setup
 
 class Attacker extends ReLogoTurtle {
 	
-	def tier = 1 // 1 - 6 based on DSB
-	def phase = 0 //1-7
-	def attacks = [] //array of Attacks
+	def tier = 1 // 1 - 6 based on Defense Science Board Report
+	def phase = 0 // 1-7 identifying which phase of kill chain agent is in
+	def attacks = [] //array of attacks available to the agent
 	
 	def setup(){
 		
